e4b3d811da929f989a1f81722a835f5336306b30
sourabh_gupta <gsourabh92@gmail.com>
2019-01-07 12:55:33 +0530

    Update admin.rb
    Add admin page elements

--- site_prism/features/lib/admin.rb
+++ site_prism/features/lib/admin.rb
@@ -1,7 +1,8 @@
-class Admin < SitePrism::Page
-  element :message, "body > div.main-wrapper > div > div"
-  def success_message
-    self.message.set "#{message}"
-  end
-
+class AdminPage < SitePrism::Page
+  element :candidate_tab, "#navbarResponsive > ul > li.nav-item.dropdown.active.show > ul > li:nth-child(2) > a"
+  element :company_user_tab, "#navbarResponsive > ul > li.nav-item.dropdown.active.show > ul > li:nth-child(3) > a"
+  element :companies_tab, "#navbarResponsive > ul > li.nav-item.dropdown.active.show > ul > li:nth-child(3) > a"
+  element :clinics_tab, "#navbarResponsive > ul > li:nth-child(3) > a"
+  element :jobs_tab, "#navbarResponsive > ul > li:nth-child(4) > a"
+  element :history_tab, "#navbarResponsive > ul > li:nth-child(5) > a"
 end
